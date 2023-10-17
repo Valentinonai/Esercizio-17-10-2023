722d6018c9fcdb2713ccf7c8705df07bc61bfa3c
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-10-17 14:11:54 +0200

    Creato metodo save

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,26 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+import org.example.entities.Evento;
+import org.example.entities.EventoDAO;
+import org.example.entities.JpaUtil;
+import org.example.entities.TipoEvento;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = JpaUtil.getEmf().createEntityManager();
+        Faker fkr = new Faker();
+        EventoDAO evdao = new EventoDAO(em);
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            evdao.save(new Evento(fkr.name().title(), LocalDate.of(rnd.nextInt(2023, 2025), rnd.nextInt(1, 13), rnd.nextInt(1, 29)), "Description", rnd.nextInt(1, 3) == 1 ? TipoEvento.PRIVATO : TipoEvento.PUBBLICO, rnd.nextInt(1, 1000)));
+        }
+
     }
 }
---
+++ src/main/java/org/example/entities/EventoDAO.java
@@ -0,0 +1,25 @@
+package org.example.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class EventoDAO {
+
+    private EntityManager em;
+
+    public EventoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Evento evento) {
+        try {
+            EntityTransaction t = em.getTransaction();
+            t.begin();
+            em.persist(evento);
+            t.commit();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
